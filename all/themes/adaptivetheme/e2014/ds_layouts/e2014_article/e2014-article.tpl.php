a35a2f291c21752420ea8d45b9dcb9650e086ac8
Johan Thomsen <jot@colabo.dk>
2013-09-28 10:30:30 +0200

    Populated repo with sites dir

---
+++ all/themes/adaptivetheme/e2014/ds_layouts/e2014_article/e2014-article.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @file
+ * Display Suite E2014 Article template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $main: Rendered content for the "Main" region.
+ * - $main_classes: String of classes that can be used to style the "Main" region.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+
+  <!-- Needed to activate contextual links -->
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+    <<?php print $main_wrapper; ?> class="ds-main<?php print $main_classes; ?>">
+      <?php print $main; ?>
+    </<?php print $main_wrapper; ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
